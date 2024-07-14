03097462d92d176c30e301f4f1e40fdb032f9c00
Shiviell <107198510+Shiviell@users.noreply.github.com>
2024-07-14 16:15:18 +0300

    Update nodejs_docker.groovy

--- basics/dsl/nodejs_docker.groovy
+++ basics/dsl/nodejs_docker.groovy
@@ -1,6 +1,6 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/yanivomc/docker-cicd.git','master') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/shiviell/docker-cicd.git','master') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@devophift.work')
         }
@@ -12,7 +12,7 @@ job('NodeJS Docker example') {
     
     steps {
         dockerBuildAndPublish {
-            repositoryName('yanivomc/amdocsapp')
+            repositoryName('shiviell/amdocsapp')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('yaniv-dockerhub')
             buildContext('./basics/')
